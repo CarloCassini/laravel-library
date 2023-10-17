b5f3c8ff16889ad32ede7195d0bbd070f9ef9143
CarloCassini <c.cassini87@gmail.com>
2023-10-17 16:11:15 +0200

    create migration for book table

---
+++ database/migrations/2023_10_17_135435_create_books_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('title', 100);
+            $table->string('author', 50);
+            $table->string('editor', 50);
+            $table->string('genre');
+            $table->text('synopsis')->nullable();
+            $table->date('published');
+            $table->integer('pages')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+};
