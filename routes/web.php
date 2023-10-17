dc31331ce384b4b9f391140057032517b8d49337
plastikSun <marcom2k01@gmail.com>
2023-10-17 17:11:30 +0200

    fix: removed extra books route

--- routes/web.php
+++ routes/web.php
@@ -19,5 +19,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [PageController::class, 'index'])->name('home');
 
 Route::get('/books', [BookController::class, 'index'])->name('books');
-
-Route::get('/books', [BookController::class, 'index'])->name('books');
