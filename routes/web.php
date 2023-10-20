7a9a4b19f30f96a7e884cfb9b881f3622de817ab
CarloCassini <c.cassini87@gmail.com>
2023-10-20 15:43:58 +0200

    add resorce book

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PageController::class, 'index'])->name('home');
 
-Route::get('/books', [BookController::class, 'index'])->name('books');
+Route::resource('index', BookController::class);
+
+// Route::get('/books', [BookController::class, 'index'])->name('books');
