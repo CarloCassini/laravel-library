cd2e67c37b99e98aa9041bb8a314e4ff4f5f2fbb
AndreaPiazza33 <andreapiazza190201@gmail.com>
2023-10-30 16:11:12 +0100

    feat: add genres table

---
+++ app/Models/Genre.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Genre extends Model
+{
+    use HasFactory;
+    public function book(){
+        return $this->hasMany(Book::class);
+    }
+}
