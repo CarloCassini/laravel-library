96fc38c3af1572b73598ba5c216405f18ab870d0
CarloCassini <c.cassini87@gmail.com>
2023-11-06 15:47:42 +0100

    crato relazione+models

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -5,12 +5,21 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+// importiamo il modello delle typology
+use App\Models\Typology;
+
 class Book extends Model
 {
     use HasFactory;
 
-    public function genre(){
+    public function genre()
+    {
         return $this->belongsTo(Genre::class);
+    }
+
+    public function typologies()
+    {
+        return $this->belongsToMany(Typology::class);
     }
 
     protected $fillable = [
